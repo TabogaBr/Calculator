e60b12f784f713ff72dd909e9f625611f28de436
TabogaBr <brenda.taboga7@gmail.com>
2022-09-01 15:28:04 +0300

    Assignment 1 (Calculator) complete

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [result, setResult] = useState('');
+  const [calc1, setCalc1] = useState('');
+  const [calc2, setCalc2] = useState('');
+  const doAddition = () => {
+    let res = parseInt(calc1) + parseInt(calc2);
+    setResult(res);
+  }
+  const doSubstraction = () => {
+    let res = parseInt(calc1) - parseInt(calc2);
+    setResult(res);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style = {styles.margins} >Result: {result} </Text>
+      <TextInput style = {[styles.textinputs, styles.margins]}
+      keyboardType = 'numeric'
+      onChangeText={calc1 => setCalc1(calc1)}
+      value={calc1}/>
+      <TextInput style = {[styles.textinputs, styles.margins]}
+      keyboardType = 'numeric'
+      onChangeText={calc2 => setCalc2(calc2)}
+      value={calc2}/>
+      <View style = {styles.buttons} >
+        <Button onPress = {doAddition} title=" + " />
+        <Button onPress = {doSubstraction} title=" - " />
+      </View>
     </View>
   );
 }
@@ -17,4 +39,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textinputs: {
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
+  buttons: {
+    flexDirection: 'row',
+  },
+  margins: {
+    margin: '3%',
+  }
 });
